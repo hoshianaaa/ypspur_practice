e2138589cb34c2690b51806d31a0427784342804
hoshianaaa <hoshina12345@gmail.com>
2019-11-21 11:40:14 +0900

    sqare move

--- src/test1.cpp
+++ src/test1.cpp
@@ -11,16 +11,32 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
   
   Spur_init();
-  YP_set_wheel_vel(13.0, 13.0);
-  YP_set_wheel_accel(13.0, 13.0);
- 
-  double x, y, th;
+
+  YPSpur_set_vel(0.5);
+  YPSpur_set_angvel(1.5);
+  YPSpur_set_accel(1.0);
+  YPSpur_set_angaccel(2.0);
+
+  YPSpur_set_pos( 0, 0, 0, 0);
+
   while(ros::ok()){
-    //YP_wheel_vel(1.0, 1.0); 
-    YPSpur_get_pos(0, &x, &y, &th);
-    std::cout << "pos GL: (" << x << ", " << y << ", " << th << ")" << std::endl;
-    YPSpur_get_pos(1, &x, &y, &th);
-    std::cout << "pos GL: (" << x << ", " << y << ", " << th << ")" << std::endl;
+
+    YPSpur_line( 0, 0, 0, 0);
+    while( !YPSpur_near_pos(0, 2, 0, 0.1))usleep(10000);
+    YPSpur_spin(0,M_PI/2);
+    while( !YPSpur_near_ang(0, M_PI/2, 0.1))usleep(10000);
+    YPSpur_line( 0, 2, 0, M_PI/2);
+    while( !YPSpur_near_pos(0, 2, 2, 0.1))usleep(10000);
+    YPSpur_spin(0,M_PI);
+    while( !YPSpur_near_ang(0, M_PI, 0.1))usleep(10000);
+    YPSpur_line( 0, 0, 2, M_PI);
+    while( !YPSpur_near_pos(0, 0, 2, 0.1))usleep(10000);
+    YPSpur_spin(0,M_PI*3/2);
+    while( !YPSpur_near_ang(0, M_PI*3/2, 0.1))usleep(10000);
+    YPSpur_line( 0, 0, 0, M_PI*3/2);
+    while( !YPSpur_near_pos(0, 0, 0, 0.1))usleep(10000);
+
+    YPSpur_freeze();
   }
   return 0;
 }
